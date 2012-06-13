af484896ddd4ee9deb642f9d1052442f3b62b8fd
Oto Brglez <otobrglez@gmail.com>
2012-06-13 15:40:49 +0200

    Update...

--- client.rb
+++ client.rb
@@ -4,11 +4,11 @@ require 'bundler/setup'
 require 'amqp'
 
 EventMachine.run do
-  connection = AMQP.connect(:host => '127.0.0.1')
-  puts "Connecting to AMQP broker. Running #{AMQP::VERSION} version of the gem..."
+  connection = AMQP.connect(:host => 'mq.politiki.si')
+  puts "Connecting to mq.politiki.si broker."
 
   channel  = AMQP::Channel.new(connection)
-  queue    = channel.queue("amqpgem.examples.hello_world", :auto_delete => true)
+  queue    = channel.queue("politiki.dev", :auto_delete => true)
 #  exchange = channel.default_exchange
 
   queue.subscribe do |payload|
