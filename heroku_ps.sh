16a98a8144c4c7fe190686774f42c9fc30c5bf24
Oto Brglez <otobrglez@gmail.com>
2012-06-22 16:09:45 +0200

    Changing config.yml

--- heroku_ps.sh
+++ heroku_ps.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-apps=(politiki-si-scheduler politiki-si-stream-twitter politiki-si-bot-twitter)
+apps=(politiki-si-scheduler politiki-si-stream-twitter politiki-si-bot-twitter politiki-si-bot-facebook)
 
 for app in ${apps[*]}
 do
